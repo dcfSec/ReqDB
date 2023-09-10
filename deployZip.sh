d8b6da515cbd32cff4537c443ba45daf3ce9364f
dcfSec <contributor@dcfsec.com>
2023-09-10 21:42:35 +0200

    Adds deployment with zip for Azure

---
+++ deployZip.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# ENV: RESOURCE_GROUP_NAME
+# ENV: APP_SERVICE_NAME
+
+cd front-end
+npm run build
+cd ..
+
+zip -r ReqDB.zip app.py requirements.txt api/ front-end/build
+
+az webapp deploy --name $APP_SERVICE_NAME --resource-group $RESOURCE_GROUP_NAME --src-path ReqDB.zip
