da741087cbb4a5cdd72a8d495ead0d5448a6b362
dcfSec <contributor@dcfsec.com>
2023-09-16 15:01:49 +0200

    Cleans up error handling

--- api/errorHandler.py
+++ api/errorHandler.py
@@ -9,7 +9,6 @@ def handle_exception(e):
     """Return JSON instead of HTML for HTTP errors."""
     # start with the correct headers and status code from the error
     response = e.get_response()
-    print("ok")
     # replace the body with JSON
     response.content_type = "application/json"
     return jsonify({
