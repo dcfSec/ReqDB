174110848ce3514d68ca2340eba829da280b33cc
dcfSec <contributor@dcfsec.com>
2023-10-19 13:52:51 +0200

    Adds function to select the signing key dynamically

--- api/__init__.py
+++ api/__init__.py
@@ -30,3 +30,18 @@ db.init_app(app)
 
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 api = ReqDBApi(api_bp)
+
+
+@jwt.decode_key_loader
+def getDecodeKey(header, payload):
+    """Returns the correct decoding key for the jwt
+
+    :param dict header: jwt header
+    :param dict payload: jwt payload
+    :raises KeyError: Returns if the key ID is not found in the dict
+    :return str: Key in PEM format
+    """
+    if header['kid'] in Config.JWT_PUBLIC_KEY:
+        return Config.JWT_PUBLIC_KEY[header['kid']]
+    else:
+        raise KeyError(f"kid {header['kid']} is not a supported key ID")
--- api/config.py
+++ api/config.py
@@ -14,18 +14,25 @@ except ImportError:
 basedir = path.abspath(path.dirname(__file__))
 
 
-def getAzureJWTKey():
+def getAzureJWTKeys():
+    """Returns the jwt signing keys from the microsoft jwk list as dictionary
+    with kid -> pem
+
+    :return dict: Dict with kid: pem
+    """
+    r = {}
     response = requests.get(
         "https://login.microsoftonline.com/common/discovery/keys"
     )
     keys = response.json()['keys']
-    rsa_pem_key = jwt.algorithms.RSAAlgorithm.from_jwk(json.dumps(keys[0]))
-    rsa_pem_key_bytes = rsa_pem_key.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    )
-
-    return rsa_pem_key_bytes
+    for key in keys:
+        rsa_pem_key = jwt.algorithms.RSAAlgorithm.from_jwk(json.dumps(key))
+        rsa_pem_key_bytes = rsa_pem_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+        r[key["kid"]] = rsa_pem_key_bytes
+    return r
 
 
 class Config:
@@ -39,4 +46,4 @@ class Config:
     JWT_DECODE_AUDIENCE = f"api://{getenv('OAUTH_APP_CLIENT_ID')}"
     JWT_DECODE_ISSUER = \
         f"https://sts.windows.net/{getenv('OAUTH_APP_TENANT')}/"
-    JWT_PUBLIC_KEY = getAzureJWTKey()
+    JWT_PUBLIC_KEY = getAzureJWTKeys()
