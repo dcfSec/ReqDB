b5c896c82eb7fdd7fa75a95b5b76186fa510d0ff
dcfSec <contributor@dcfsec.com>
2024-08-28 11:42:57 +0200

    Cleans up requirement view

--- front-end/src/pages/Requirement.jsx
+++ front-end/src/pages/Requirement.jsx
@@ -1,22 +1,16 @@
-import { Alert, Badge, Card, Col, Container, Row, Form } from "react-bootstrap";
-import { MainBreadcrumb } from "../components/MiniComponents";
-import CommentEntry from "../components/Comments/CommentEntry";
-import AddComment from "../components/Comments/AddComment";
+import { Alert, Col, Container, Row } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import { ErrorMessage } from '../components/MiniComponents'
-import { appRoles } from '../authConfig';
 import LoadingBar from "../components/LoadingBar";
 
 import { protectedResources } from "../authConfig";
 import useFetchWithMsal from '../hooks/useFetchWithMsal';
 import { useParams } from "react-router-dom";
-import { ReactMarkdown } from "react-markdown/lib/react-markdown";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 import { useDispatch, useSelector } from 'react-redux'
 import { showSpinner } from "../stateSlices/MainLogoSpinnerSlice";
 import { reset, setRequirement } from "../stateSlices/RequirementSlice";
 import { setBreadcrumbs, setPageTitle } from "../stateSlices/LayoutSlice";
+import { CommentCard, DescriptionCard, ExtraCard, TagsCard, TopicsCard } from "../components/Requirement/RequirementCards";
 
 /**
  * View to select a catalogue to browse in the BrowseCatalogue view
@@ -31,14 +25,10 @@ export default function Requirement() {
     dispatch(setBreadcrumbs([{ href: "/Browse", title: "Browse", active: false }, { href: "", title: "View Requirement", active: true }]))
   }, []);
 
-  const roles = useSelector(state => state.user.roles)
-  const requirement = useSelector(state => state.requirement.requirement)
   const title = useSelector(state => state.layout.pageTitle)
 
   const [fetched, setFetched] = useState(false);
   const [APIError, setAPIError] = useState(null);
-
-  const [showCompleted, setShowCompleted] = useState(false);
 
   const params = useParams();
   const id = params.requirementId
@@ -71,81 +61,31 @@ export default function Requirement() {
   } else if (APIError) {
     body = <Alert variant="danger">{ErrorMessage(APIError)}</Alert>
   } else if (fetched) {
-    const completedCount = [...requirement.comments].filter((el) => el.completed == true).length
     body = <Container>
       <Row>
         <Col>
-          <Card>
-            <Card.Header as="h3">Tags</Card.Header>
-            <Card.Body>
-              <Card.Text>{[...requirement.tags].map(tag => (<span key={tag.id}><Badge bg="secondary">{tag.name}</Badge>{' '}</span>))}</Card.Text>
-            </Card.Body>
-          </Card>
+          <TagsCard />
         </Col>
         <Col>
-          <Card>
-            <Card.Header as="h3">Topics</Card.Header>
-            <Card.Body>
-              <Card.Text>{buildTopicsList({ ...requirement.parent }).map(topic => (<span key={topic}>{' '}<FontAwesomeIcon icon={solid("arrow-right")} />{' '}<Badge bg="secondary">{topic}</Badge></span>))}</Card.Text>
-            </Card.Body>
-          </Card>
+          <TopicsCard />
         </Col>
       </Row>
       <Row>
         <Col>
-          <Card>
-            <Card.Header as="h3">Description</Card.Header>
-            <Card.Body>
-              <ReactMarkdown className="card-text">{requirement.description}</ReactMarkdown>
-            </Card.Body>
-          </Card>
+          <DescriptionCard />
         </Col>
       </Row>
       <Row>
         <Col>
-          <Card>
-            <Card.Header as="h3">Extras</Card.Header>
-            <Card.Body>
-              {[...requirement.extras].map(extra => (<span key={extra.id} ><Card.Title>{extra.extraType.title}</Card.Title>{printExtraWithType(extra.extraType.extraType, extra.content)}</span>))}
-            </Card.Body>
-          </Card>
+          <ExtraCard />
         </Col>
       </Row>
-      {roles.includes(appRoles.Comments.Reader) || roles.includes(appRoles.Comments.Writer) ?
-        <Row>
-          <Col>
-            <Card>
-              <Card.Header as="h3">Comments</Card.Header>
-              <Card.Body>
-                {completedCount > 0 ? <Form.Check type="switch" id="completed" defaultChecked={showCompleted} onChange={e => { setShowCompleted(e.target.checked) }} label={`${completedCount} comments completed. Show completed`} reverse /> : null}
-                {roles.includes(appRoles.Comments.Reader) ? [...requirement.comments].sort((a, b) => a.id - b.id).map((item, commentIndex) => <CommentEntry view={"requirement"} rowIndex={null} commentIndex={commentIndex} comment={item} key={`comment-${commentIndex}`} showCompleted={showCompleted} />) : null}
-                {roles.includes(appRoles.Comments.Writer) ? <><Card.Title>Add Comment</Card.Title><AddComment view={"requirement"} index={null} requirementId={requirement["id"]} /></> : null}
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row> : null
-      }
+      <Row>
+        <Col>
+          <CommentCard />
+        </Col>
+      </Row>
     </Container>
-  }
-
-  function buildTopicsList(topic) {
-    let r = [topic.title]
-    if (topic.parent !== null) {
-      r = buildTopicsList(topic.parent).concat(r)
-    }
-    return r
-  }
-
-  function printExtraWithType(extraType, content) {
-    if (extraType === 1) {
-      return <p>{content}</p>
-    } else if (extraType === 2) {
-      return <ReactMarkdown>{content}</ReactMarkdown>
-    } else if (extraType === 3) {
-      return <p>{content}</p>
-    } else {
-      return <></>
-    }
   }
 
   return (
