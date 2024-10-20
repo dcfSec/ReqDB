0618105ef37965bac7be761f1ce098ded8a9a1ab
dcfSec <contributor@dcfsec.com>
2024-10-20 23:42:09 +0200

    Fixes roles check

--- api/helper.py
+++ api/helper.py
@@ -3,14 +3,7 @@ from flask_jwt_extended import get_jwt
 
 def checkAccess(jwt, neededRoles):
     if 'roles' not in jwt or not (set(jwt['roles']) & set(neededRoles)):
-        abort(401, {
-            "status": "401",
-            "name": "AuthorizationError",
-            "data": {
-                "error": "NotPermitted",
-                "message": "User is not permitted to access this resource"
-            },
-        })
+        abort(401, "Missing role")
 
 def getUserUPN():
     return get_jwt()["upn"]
