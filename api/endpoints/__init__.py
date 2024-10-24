3d89eb80cc2b77691d5708b015912fbd5d45a3aa
dcfSec <contributor@dcfsec.com>
2024-10-24 15:14:28 +0200

    Adds check if user is in DB for auditing

--- api/endpoints/__init__.py
+++ api/endpoints/__init__.py
@@ -4,7 +4,7 @@ Path mapping for the API.
 Maps classes to the API paths
 """
 
-from api.appDefinition import api
+from api.appDefinition import api_bp, api, db
 
 from api.endpoints.catalogues import Catalogue, Catalogues
 from api.endpoints.comments import Comment, Comments
@@ -16,6 +16,10 @@ from api.endpoints.topics import Topic, Topics
 from api.endpoints.coffee import Coffee
 from api.endpoints.wildcard import Wildcard
 from api.endpoints.audit import Audit
+
+from api.helper import getUserUPN
+from api.models import User
+from flask_jwt_extended import jwt_required
 
 api.add_resource(Catalogues, '/catalogues')
 api.add_resource(Catalogue, '/catalogues/<int:id>')
@@ -43,3 +47,12 @@ api.add_resource(Coffee, '/coffee')
 api.add_resource(Audit, '/audit/<string:object>/<int:id>', '/audit/<string:object>')
 
 api.add_resource(Wildcard, '', '/', '/<path:path>')
+
+
+@api_bp.before_request
+@jwt_required()
+def checkUserInDBorCreate():
+    user = User.query.get(getUserUPN())
+    if user is None:
+        db.session.add(User(id=getUserUPN()))
+        db.session.commit()
