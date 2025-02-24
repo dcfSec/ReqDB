01c01572026442776112f0f3080c3746569afa8d
dcfSec <contributor@dcfsec.com>
2025-02-24 19:23:00 +0100

    Adds chunk config for building

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import macrosPlugin from "vite-plugin-babel-macros";
+import { dependencies } from './package.json';
+//import { nodePolyfills } from 'vite-plugin-node-polyfills'
+
+function renderChunks(deps: Record<string, string>) {
+  const chunks: Record<string, string[]> = {};
+  Object.keys(deps).forEach((key) => {
+    if (['react', 'react-router-dom', 'react-dom'].includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +23,7 @@ export default defineConfig({
       '/api': "http://localhost:8000",
     }
   },
-  plugins: [react(), macrosPlugin()],
+  plugins: [react(), macrosPlugin(), /*nodePolyfills()*/],
   define: {
     'process.env': process.env,
     'process.platform': JSON.stringify(process.version),
@@ -22,5 +34,15 @@ export default defineConfig({
     supported: {
       'top-level-await': true
     },
-  }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-router-dom', 'react-dom'],
+          ...renderChunks(dependencies),
+        },
+      },
+    },
+  },
 })
