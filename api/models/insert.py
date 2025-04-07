246ae8e4052edafb98450fc347ae335b112ff942
dcfSec <contributor@dcfsec.com>
2025-04-07 16:37:44 +0200

    Fixes insert models default values

--- api/models/insert.py
+++ api/models/insert.py
@@ -5,30 +5,30 @@ from sqlmodel import SQLModel
 class Insert:
     class Tag(SQLModel):
         model_config = ConfigDict(from_attributes=True)
-        name: str | None = None
+        name: str
         catalogues: list["Insert.TagCatalogue"] = []
         requirements: list["Insert.TagRequirement"] = []
 
     class Catalogue(SQLModel):
         model_config = ConfigDict(from_attributes=True)
-        title: str | None = None
-        description: str | None = None
+        title: str
+        description: str = ""
         topics: list["Insert.CatalogueTopic"] = []
         tags: list["Insert.RequirementTag"] = []
 
     class Comment(SQLModel):
         model_config = ConfigDict(from_attributes=True)
-        comment: str | None = None
+        comment: str
         completed: bool = False
         requirementId: int
-        authorId: str | None = None
-        parentId: int | None = None
+        authorId: str
+        parentId: int
 
     class Topic(SQLModel):
         model_config = ConfigDict(from_attributes=True)
-        key: str | None = None
-        title: str | None = None
-        description: str | None = None
+        key: str
+        title: str
+        description: str = ""
         parentId: int | None = None
 
     class Requirement(SQLModel):
@@ -42,15 +42,15 @@ class Insert:
 
     class ExtraType(SQLModel):
         model_config = ConfigDict(from_attributes=True)
-        title: str | None = None
-        description: str | None = None
-        extraType: int | None = None
+        title: str
+        description: str = ""
+        extraType: int
 
     class ExtraEntry(SQLModel):
         model_config = ConfigDict(from_attributes=True)
-        content: str | None = None
-        extraTypeId: int | None = None
-        requirementId: int | None = None
+        content: str
+        extraTypeId: int
+        requirementId: int
 
     class CatalogueTopic(SQLModel):
         model_config = ConfigDict(from_attributes=True)
