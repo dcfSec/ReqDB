9e13c187aa4e1e92b60bc9f6802d39ea791ee94e
dcfSec <contributor@dcfsec.com>
2025-12-07 15:24:14 +0100

    Adds lint ignore for babel config

--- spa/babel.config.js
+++ spa/babel.config.js
@@ -1,6 +1,7 @@
+// eslint-disable-next-line no-undef
 module.exports = function (api) {
-    api.cache(true);
-    return {
-      plugins: ['macros'],
-    }
+  api.cache(true);
+  return {
+    plugins: ['macros'],
   }
+}
